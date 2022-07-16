e355887530a0a22bd42b7fa1959bc284454575be
tathapachon <85940447+tathapachon@users.noreply.github.com>
2022-07-16 09:49:18 -0500

    conexion api y modelo tickets

---
+++ api/src/models/Tickets.js
@@ -0,0 +1,42 @@
+const { DataTypes, UUIDV4 } = require("sequelize");
+module.exports = (sequelize) => {
+    sequelize.define('tickets', {
+        id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            defaultValue: UUIDV4,
+            unique: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING(),
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        image: { 
+            type: DataTypes.STRING,          
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.ENUM("active", "inactive"),
+            defaultValue: "active"
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        created: { 
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    },
+        { timestamps: false })
+
+};
---
+++ api/src/routes/ticketBasket.js
@@ -0,0 +1,16 @@
+const { Tickets } = require("../db");
+const { Router } = require("express");
+
+
+const router = Router();
+router.get("/", async (req, res) => {
+    try {
+      const allTickets = await Tickets.findAll({
+        
+      });
+      res.status(200).send("Estos son los tickets");
+    } catch (err) {
+      res.status(400).send({ msg: err.message });
+    }
+  });
+module.exports = router;
