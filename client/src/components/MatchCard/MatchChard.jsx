77fcca73a4e379b139521a7feb5287aa34e9710c
Nicole Quilmore <nicolequilmore96@gmail.com>
2022-07-18 22:22:40 -0300

    json de productos

--- client/src/components/MatchCard/MatchChard.jsx
+++ client/src/components/MatchCard/MatchChard.jsx
@@ -1,19 +1,18 @@
 import React from "react";
+import './MatchCard.css'
 
 export default function MatchCard({id, image_home, home, guest, image_guest, statistics, date}){
     const score = statistics.map(s => s.score)
     console.log(score);
     return (
-        <div>
-            <div>
+        <div className="container">
+            <div className="titleContainer">
                 <h2>{home}</h2>
                 <img src={`${image_home}`} alt={`${home}`}/>
-            </div>
-            <div>
                 <h2>{guest}</h2>
                 <img src={`${image_guest}`} alt={`${guest}`}/>
             </div>
-            <div>
+            <div className="infoContainer">
                 <h4>{score[0]} - {score[1]}</h4>
                 <h4>{date}</h4>
             </div>
