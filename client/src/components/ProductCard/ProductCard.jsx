c0484be468af0dd01e49240d09ac38306ee920b1
tathapachon <85940447+tathapachon@users.noreply.github.com>
2022-07-19 10:20:15 -0500

    Pull Nicole

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,27 +1,15 @@
-import "./ProductCard.scss"
 
-
-export default function ProductCard({ name, price, description, image, image2, image3, image4 }) {
-    return (
-        <div className="card">
-
-        <img src={image} alt="not found"/>
-        {
-          image2 !== "null" ? <img src={image2}
-        }
-  {/* <img className="imagenOne" src={`${image}`} alt={`${name}`} width={"200px"} height={"250px"} /> */}
-  {/* <img src={loadImage("image")} alt="img" /> */}
-         <div>
-
-            <h2>{name} </h2>
-            <h3>{price} </h3>
-            <h4>{description} </h4>
-         </div>
-      
-            
-            
-
-
-
-        </div>)
+export default function ProductCard({ name, price, description, image, categories}) {
+  return (
+      <div className="card">
+         <img className="imagenOne" src={image} alt="not found"/>
+          <div className="textContainer">
+            <div className="headerContainer">
+               <h2>{name} </h2>
+               <h3>AR${price} </h3>
+            </div>
+            <h6>{categories[0]} <br/> {categories[1]}</h6>
+          </div>
+      </div>
+      )
 }
