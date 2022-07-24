a55e4202c0700953b008e3a01539066247f96a66
Franco Ezequiel Ibañez <francoeibaez@gmail.com>
2022-07-24 10:00:15 -0300

    Simplifiqué el paginado

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,60 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Pagination.scss'
 
 function Pagination({ pages, setCurrentPage }) {
 
 
-    // console.log(`Soy numberOfPages ${numberOfPages}`)
-
     const [currentButton, setCurrentButton] = useState(1);
-    const [arrOfCurrentButtons, setArrOfCurrentButtons] = useState([]);
+
 
     let numberOfPages = [];
     for (let i = 1; i <= pages; i++) {
         numberOfPages.push(i);
     }
-    
-    useEffect(() => {
 
-        let tempNumberOfPages = [...arrOfCurrentButtons]
-        // console.log(`Soy tempNumberOfPages ${tempNumberOfPages}`)
-        const dotsInitial = '...'
-        const dotsLeft = '...'
-        const dotsRight = '...'
+    setCurrentPage(currentButton)
 
-        if (numberOfPages.length < 6) {
-            tempNumberOfPages = numberOfPages;
-        }
-        else if (currentButton >= 1 && currentButton <= 3) {
-            tempNumberOfPages = [1, 2, 3, 4, dotsInitial, numberOfPages.length]
-        }
-        else if (currentButton === 4) {
-            const sliced = numberOfPages.slice(0, 5);
-            tempNumberOfPages = [...sliced, dotsInitial, numberOfPages.length]
-        }
-        else if (currentButton > 4 && currentButton < numberOfPages.length - 2) {
-            const sliced1 = numberOfPages.slice(currentButton - 2, currentButton)
-            const sliced2 = numberOfPages.slice(currentButton, currentButton + 1)
-            tempNumberOfPages = ([1, dotsLeft, ...sliced1, ...sliced2, dotsRight, numberOfPages.length])
-        }
-        else if (currentButton > numberOfPages.length - 3) {
-            const sliced = numberOfPages.slice(numberOfPages.length - 4)
-            tempNumberOfPages = ([1, dotsLeft, ...sliced])
-        }
-        else if (currentButton === dotsInitial) {
-            setCurrentButton(arrOfCurrentButtons[arrOfCurrentButtons.length - 3] + 1)
-        }
-        else if (currentButton === dotsRight) {
-            setCurrentButton(arrOfCurrentButtons[3] + 2)
-        }
-        else if (currentButton === dotsLeft) {
-            setCurrentButton(arrOfCurrentButtons[3] - 2)
-        }
-
-        setArrOfCurrentButtons(tempNumberOfPages)
-        setCurrentPage(currentButton)
-
-    }, [numberOfPages])
 
     return (
         <div className="pagination-container">
@@ -65,19 +24,8 @@ function Pagination({ pages, setCurrentPage }) {
                 Prev
             </button>
 
-            {arrOfCurrentButtons.map((item, index) => {
-                return (
-                    <button
-                        key={index}
-                        className={`${currentButton === item ? 'active' : ''}`}
-                        onClick={() => setCurrentButton(item)}
+            <button>{currentButton}</button>
 
-                    >
-                        {item}
-                    </button>
-
-                )
-            })}
 
             <button
                 className={`${currentButton === numberOfPages.length ? 'disabled' : ''}`}
