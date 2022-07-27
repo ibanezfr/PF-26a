7ca04fc9814df21d142f4e0660df150655b44ecd
Franco Ezequiel Ibañez <francoeibaez@gmail.com>
2022-07-27 16:41:39 -0300

    El formulario de creación de producto casi terminado, no controlado

--- client/src/components/ProductCreationForm/ProductCreationForm.jsx
+++ client/src/components/ProductCreationForm/ProductCreationForm.jsx
@@ -3,17 +3,34 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import FileBase from "react-file-base64";
-
+//   {
+//         "name": "Bochas",
+//         "description": "Encontrá estas bochas en nuestro local o compralas por nuestra tienda virtual para poder potenciar tu entrenamiento al máximo",
+//         "price": 500,
+//         "rating": 5,
+//         "color": "rosa",
+//         "image": "https://i.pinimg.com/originals/8c/64/12/8c64122297b0ea576b1d9fdbf0673875.jpg",
+//         "categories": [
+//             {
+//                 "name": "Equipamento deportivo"
+//             }
+//         ],
+//         "product_values": [
+//             {
+//                 "size": "único",
+//                 "stock": 40
+//             }
+//         ]
+//     }
 export function validate() {
 
 }
 
 export default function ProductCreationForm() {
-    const dispatch = useDispatch();
     const history = useHistory();
     const categoriesArray = useSelector((state) => state.categories);
-    const sizesArray = ["S", "L", "M", "XL", "XXL"]
-    console.log(categoriesArray);
+    const sizesArray = ["xs", "s", "l", "m", "xl", "xxl", "xxxl", "único"]
+    // console.log(categoriesArray);
     const [input, setInput] = useState({
         name: "",
         price: 0,
@@ -25,12 +42,13 @@ export default function ProductCreationForm() {
         image4: "",
         stock: 0,
         status: "",
-        size: [],
         categories: [],
+        products_values: [],
     })
-    const [errors, setErrors] = useState({});
+    // const [errors, setErrors] = useState({});
     const handleInputChange = (e) => {
-
+        console.log(`El name del e: ${e.target.name}`);
+        console.log(`El value del e: ${e.target.value}`);
     }
     const handleSubmit = (e) => {
 
@@ -44,51 +62,57 @@ export default function ProductCreationForm() {
                 <p className="error">
                     {/* { && <span className="Error"></span>} */}
                 </p>
-                <form onSubmit={(e) => handleSubmit(e)}>
+                <form onSubmit={((e) => handleSubmit(e))}>
                     <input
                         className="form-input"
                         type="text"
                         placeholder="Your product's name..."
                         name="name"
-                        onChange={(e) => handleInputChange(e)}
+                        onChange={(e) => setInput({ ...input, name: e.target.value })}
                     ></input>
                     <input
                         className="form-input"
                         type="number"
                         placeholder="Your product's price..."
                         name="price"
-                        onChange={(e) => handleInputChange(e)}
+                        min="0"
+                        max="50000"
+                        onChange={(e) => setInput({ ...input, price: e.target.value })}
                     ></input>
                     <input
                         className="form-input"
                         type="text"
                         placeholder="Your product's color..."
                         name="color"
-                        onChange={(e) => handleInputChange(e)}
+                        onChange={(e) => setInput({ ...input, color: e.target.value })}
                     ></input>
                     <label >Image 1: </label>
                     <FileBase
+                        name="image1"
                         type="image"
                         multiple={false}
-                        onDone={({ base64 }) => setInput({ ...input, image: base64 })}
+                        onDone={({ FileBase }) => setInput({ ...input, image: FileBase })}
                     />
                     <label >Image 2: </label>
                     <FileBase
+                        name="image2"
                         type="image"
                         multiple={false}
-                        onDone={({ base64 }) => setInput({ ...input, image2: base64 })}
+                        onDone={(e) => handleInputChange(e)}
                     />
                     <label >Image 3: </label>
                     <FileBase
+                        name="image3"
                         type="image"
                         multiple={false}
-                        onDone={({ base64 }) => setInput({ ...input, image3: base64 })}
+                        onDone={(e) => handleInputChange(e)}
                     />
                     <label >Image 4: </label>
                     <FileBase
+                        name="image4"
                         type="image"
                         multiple={false}
-                        onDone={({ base64 }) => setInput({ ...input, image4: base64 })}
+                        onDone={(e) => handleInputChange(e)}
                     />
                     <input
                         className="form-input"
@@ -97,17 +121,37 @@ export default function ProductCreationForm() {
                         name="stock"
                         onChange={(e) => handleInputChange(e)}
                     ></input>
+                    <label>Categories:</label>
+                    <select
+                        className="form-input"
+                        name="categories"
+                        onChange={(e) => setInput({ ...input, categories: [...input.categories, e.target.value] })}>
+                        <option key={"21a"}>--Choose categories--</option>
+                        {
+                            categoriesArray && categoriesArray?.map((elm, index) => {
+                                return (
+                                    <option
+                                        value={elm}
+                                        key={index}
+                                    >
+                                        {elm}
+                                    </option>
+                                )
+                            })
+                        }
+                    </select>
                     <label>Size:</label>
                     <select
                         className="form-input"
                         name="size"
                         onChange={(e) => handleInputChange(e)}>
-                        <option>--Choose sizes--</option>
+                        <option key={"22a"}>--Choose sizes--</option>
                         {
-                            sizesArray && sizesArray?.map((elm) => {
+                            sizesArray && sizesArray?.map((elm, index) => {
                                 return (
                                     <option
                                         value={elm}
+                                        key={index}
                                     >
                                         {elm}
                                     </option>
@@ -115,24 +159,14 @@ export default function ProductCreationForm() {
                             })
                         }
                     </select>
-                    <label>Categories:</label>
-                    <select
-                        className="form-input"
-                        name="categories"
-                        onChange={(e) => handleInputChange(e)}>
-                        <option>--Choose categories--</option>
-                        {
-                            categoriesArray && categoriesArray?.map((elm) => {
-                                return (
-                                    <option
-                                        value={elm}
-                                    >
-                                        {elm}
-                                    </option>
-                                )
-                            })
-                        }
-                    </select>
+                    <label>Stock:</label>
+                    <input
+                        type="number"
+                        min="0"
+                        max="100"
+                        onChange={(e) => handleInputChange(e)}
+                    >
+                    </input>
                 </form>
             </section>
         </div >
