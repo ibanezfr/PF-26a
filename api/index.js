96f08a3358fbee2ea64b58242e730e61285d6dd7
Nicole Quilmore <nicolequilmore96@gmail.com>
2022-07-27 23:13:59 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -2,9 +2,15 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { getProducts } = require("./src/middlewares/middlewares");
 
+// conn.sync({ force: true }).then(() => {
+//   server.listen(3001, async () => {
+//     await getProducts();
+//     console.log("%s listening at 3001");
+//   });
+// });
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, async () => {
-    await getProducts();
-    console.log("%s listening at 3001");
+  server.listen(process.env.PORT, async () => {
+      await getProducts();
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
