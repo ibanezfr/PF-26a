8253b9e96772fa3e437caf7d24834f8fcc7748c4
Micaias <gutierrezmicaias@gmail.com>
2022-07-29 18:04:52 -0300

    empezando con los Favs

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,12 +3,14 @@ const products = require("./products.js");
 const categories = require("./categories");
 const functionalities = require('./functionalities');
 const usersHandlers = require('./users.js')
-const pay= require("./pay")
+const pay = require("./pay")
+const favorites = require("./favorites")
 
 
 router.use("/products", products);
 router.use("/categories", categories);
-router.use("/pay", pay)
+router.use("/pay", pay);
+router.use("/favs", favorites);
 
 // Usuarios ->
 const user = require("./user.js");
