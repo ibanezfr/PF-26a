eb5c632c5639e0cff1f0d11cb5b222ad7fda7e31
Franco Ezequiel Ibañez <francoeibaez@gmail.com>
2022-08-08 13:55:06 -0300

    habia un error en la traducción de widget en dashboard

--- client/src/components/AdminComponents/Widget/Widget.jsx
+++ client/src/components/AdminComponents/Widget/Widget.jsx
@@ -24,27 +24,27 @@ const Widget = ({ type, amount, ordersNumber }) => {
 
     case "order":
       data = {
-        title: t('widget.user.title'),
+        title: t('widget.order.title'),
         isMoney: false,
-        link: t('widget.user.link'),
+        link: t('widget.order.link'),
         icon: <FaShoppingCart className="icon" />,
       };
       break;
 
     case "earning":
       data = {
-        title: t('widget.user.title'),
+        title: t('widget.earning.title'),
         isMoney: true,
-        link: t('widget.user.link'),
+        link: t('widget.earning.link'),
         icon: <MdMonetizationOn className="icon" />,
       };
       break;
 
     case "balance":
       data = {
-        title: t('widget.user.title'),
+        title: t('widget.balance.title'),
         isMoney: true,
-        link: t('widget.user.link'),
+        link: t('widget.balance.link'),
         icon: <MdAccountBalance className="icon" />,
       };
       break;
